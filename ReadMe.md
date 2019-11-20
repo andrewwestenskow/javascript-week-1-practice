71feb49cb465413426e0f1abeaaf9690ce9052fe
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-11-20 14:49:26 -0700

    Added day 1 practice

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# JavaScript week 1 additional practice.
+
+---
+
+### Instructions:
+
+Find the file that corresponds to today's lecture. Copy the file into a repl.it and work through the toy problems.
+
+> Note: These problems have no tests associated with them, you are responsible to make sure they work as expected.
---
+++ javascript1.js
@@ -0,0 +1,111 @@
+//Copy this file into a repl.it and work through the toy problems.
+//There are no tests so you are responsible for making sure the functions act as expected
+
+/*
+Write a function called add that takes two parameters (i.e. num1, num2), the function should return the sum of the two numbers
+
+sum(1,2) should return 3
+
+*/
+
+//CODE HERE
+
+/*
+Write a function called subtract that takes two parameters (i.e. num1, num2), the function should return the difference of the two numbers
+
+subtract(5,4) should return 1
+
+*/
+
+//CODE HERE
+
+/*
+Write a function called multiply that takes two parameters (i.e. num1, num2), the function should return the product of the two numbers
+
+multiply(5,5) should return 25
+
+*/
+
+//CODE HERE
+
+/*
+Write a function called divide that takes two parameters (i.e. num1, num2), the function should return the quotient of the two numbers
+
+divide(10,5) should return 2
+
+*/
+
+//CODE HERE
+
+/*
+Write function bmi that takes in two parameters, height and weight and calculates body mass index (bmi = weight / height ^ 2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+
+//CODE HERE
+
+/*
+Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+*/
+
+//CODE HERE
+
+/*
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+If the name is the same as the owner, it should return 'Hello, boss!'
+IF the name is different from the owner, it should return 'Hello, guest!'
+*/
+
+//CODE HERE
+
+/*
+Create a function called shoppingList that takes 3 arguments, all items to be added to a shopping list.
+The function should return all of the arguments as an array
+
+For example shoppingList('butter', 'milk', 'cheese') should return ['butter', 'milk', 'cheese']
+
+*/
+
+//CODE HERE
+
+/*
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+Write a function that takes in the amount of time in hours that Nathan will spend cycling and returns how much water he will drink in litres.
+
+For example:
+
+time = 3 ----> litres = 1.5
+
+time = 6 ---> litres = 3
+
+*/
+
+//CODE HERE
+
+/*
+Write a function called makeUpperCase that takes in a string and returns the uppercased version of that string.  For example:
+
+makeUpperCase('andrew') should return 'ANDREW'
+
+*/
+
+/*
+Write a function called personBuilder that takes the following arguments: name (a string), age (a number), married (a boolean), pets (an array) and returns an object with matching properties.  
+
+*/
+
+//CODE HERE
